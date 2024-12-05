a8142063ab2b089a542f67148770364f7e31e211
de Monasterio <112005353+DeMonasterio@users.noreply.github.com>
2024-12-05 13:01:34 +0000

    Added postController methods

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,16 +1,17 @@
 const Post = require('../models/Post');
+
 const getPosts = async (req, res) => {
     try {
+        
         const posts = await Post.find()
             .populate('author', 'username email')
             .sort({ createdAt: -1 });
 
-        // Convertir la imagen a base64
         const postsWithBase64Images = posts.map(post => {
             if (post.image) {
                 return {
                     ...post.toObject(),
-                    image: post.image.toString('base64'), // Convierte el buffer a base64
+                    image: post.image.toString('base64'),
                 };
             }
             return post;
@@ -23,20 +24,44 @@ const getPosts = async (req, res) => {
     }
 };
 
-const getPostsByUserId = async (req, res) => {
-    const { userId } = req.params; // Obtenemos el userId de los parámetros de la ruta
+const getPostById = async (req, res) => {
+    const { id } = req.params;
 
     try {
-        const posts = await Post.find({ author: userId }) // Busca publicaciones por author
-            .populate('author', 'username email') // Rellena los datos del autor
-            .sort({ createdAt: -1 }); // Ordena las publicaciones por fecha
+        const post = await Post.findById(id)
+            .populate('author', 'username email');
 
-        // Convertir la imagen a base64
+        if (!post) {
+            return res.status(404).json({ error: 'Post not found' });
+        }
+
+        const postWithBase64Image = post.image
+            ? {
+                  ...post.toObject(),
+                  image: post.image.toString('base64'),
+              }
+            : post.toObject();
+
+        res.status(200).json(postWithBase64Image);
+    } catch (error) {
+        console.error('Error fetching post:', error);
+        res.status(500).json({ error: 'Error fetching post' });
+    }
+};
+
+const getPostsByUserId = async (req, res) => {
+    const { userId } = req.params;
+
+    try {
+        const posts = await Post.find({ author: userId })
+            .populate('author', 'username email')
+            .sort({ createdAt: -1 });
+
         const postsWithBase64Images = posts.map(post => {
             if (post.image) {
                 return {
                     ...post.toObject(),
-                    image: post.image.toString('base64'), // Convierte el buffer a base64
+                    image: post.image.toString('base64'),
                 };
             }
             return post;
@@ -54,8 +79,6 @@ const uploadFile = async (req, res) => {
         if (!req.file) {
             return res.status(400).json({ message: 'No file uploaded' });
         }
-        
-        
 
         const { title, content, author, theme } = req.body;
 
@@ -66,7 +89,7 @@ const uploadFile = async (req, res) => {
         const newPost = new Post({
             title,
             content,
-            author, 
+            author,
             theme,
             image: req.file.buffer,
         });
@@ -80,4 +103,23 @@ const uploadFile = async (req, res) => {
     }
 };
 
-module.exports = { getPosts, getPostsByUserId, uploadFile };
+// Método para eliminar una publicación
+const deletePost = async (req, res) => {
+    const { id } = req.params;
+    console.log("Hola")
+
+    try {
+        const post = await Post.findByIdAndDelete(id);
+
+        if (!post) {
+            return res.status(404).json({ error: 'Post not found' });
+        }
+
+        res.status(200).json({ message: 'Post deleted successfully', post });
+    } catch (error) {
+        console.error('Error deleting post:', error);
+        res.status(500).json({ error: 'Error deleting post' });
+    }
+};
+
+module.exports = { getPosts, getPostById, getPostsByUserId, uploadFile, deletePost };
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,9 +2,11 @@
 const express = require('express');
 const router = express.Router();
 const upload = require('../config/multer');
-const {  getPosts, getPostsByUserId,uploadFile } = require('../controllers/postController');
+const {  getPosts, getPostById, getPostsByUserId, uploadFile, deletePost } = require('../controllers/postController');
 router.get('/get', getPosts); 
+router.get('/get/:id', getPostById); 
 router.get('/get/user/:userId', getPostsByUserId);
 router.post('/upload', upload.single('image'), uploadFile);
+router.delete('/delete/:id', deletePost);
 
 module.exports = router;
